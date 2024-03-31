b51338e26fee11c5baf50701e2a305286f88d6dc
polandasish <139715583+polandasish@users.noreply.github.com>
2024-03-31 22:41:53 +0200

    Update provider.tf

--- eks_setup/provider.tf
+++ eks_setup/provider.tf
@@ -4,12 +4,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.42.0"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.27.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.region
 }
+
+
+
 
 
 

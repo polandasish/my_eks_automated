e34179d3e4952932fe0336225c8b0daa665e197a
polandasish <139715583+polandasish@users.noreply.github.com>
2024-03-31 22:50:46 +0200

    Update script.sh

--- jenkis_setup/script.sh
+++ jenkis_setup/script.sh
@@ -42,3 +42,26 @@ sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+## Install Docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+
+sudo apt update -y
+
+sudo apt-cache policy docker-ce -y
+
+sudo apt install docker-ce -y
+
+#sudo systemctl status docker
+
+sudo chmod 777 /var/run/docker.sock
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+sudo apt-get update -y

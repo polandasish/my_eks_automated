ef716c4d8513d3efa65577ecf389dc0ce1b8152f
polandasish <139715583+polandasish@users.noreply.github.com>
2024-04-01 15:14:47 +0200

    Update data.tf

--- eks_setup/data.tf
+++ eks_setup/data.tf
@@ -1,1 +1,12 @@
 data "aws_availability_zones" "azs" {}
+
+data "aws_eks_cluster" "cluster" {
+  name= var.cluster_name
+  depends_on = [ module.eks.cluster_name ]
+  
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name= var.cluster_name
+  depends_on = [ module.eks.cluster_name ]
+  
+}

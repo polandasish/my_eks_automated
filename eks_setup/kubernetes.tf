a3a19f96c82b7a8b85557f745af2ee4add14fd33
polandasish <139715583+polandasish@users.noreply.github.com>
2024-04-01 11:59:03 +0200

    Update kubernetes.tf

--- eks_setup/kubernetes.tf
+++ eks_setup/kubernetes.tf
@@ -1,9 +1,20 @@
-/*
+
+data "aws_eks_cluster" "cluster" {
+  name= var.cluster_name
+  depends_on = [ module.eks.cluster_name ]
+  
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name= var.cluster_name
+  depends_on = [ module.eks.cluster_name ]
+  
+}
+
+
 provider "kubernetes" {
-   host = data.aws_eks_cluster.cluster.endpoint
-   token= data.aws_eks_cluster_auth.cluster.token
-   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+   host = data.aws_eks_cluster.my-eks-cluster.endpoint
+   token= data.aws_eks_cluster_auth.my-eks-cluster.token
+   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 
   
 }
-*/
